3debef5a338b40891d61ffd7257bbd6b3b3333bd
LenaWB-SOE <170402755+LenaWB-SOE@users.noreply.github.com>
2024-11-07 14:22:54 +0000

    using env instead

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
-# CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-# CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
-# REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
+CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 
-CLIENT_ID = "362a407b4fc34e7abc359cfc77626479"
-CLIENT_SECRET = "a4c35e2f1a3f4bf285ed264d83f93029"
-REDIRECT_URI = 'http://localhost:5000/callback'
+# CLIENT_ID = "362a407b4fc34e7abc359cfc77626479"
+# CLIENT_SECRET = "a4c35e2f1a3f4bf285ed264d83f93029"
+# REDIRECT_URI = 'http://localhost:5000/callback'
 AUTH_URL = 'https://accounts.spotify.com/authorize'
 TOKEN_URL = 'https://accounts.spotify.com/api/token'
 API_BASE_URL = 'https://api.spotify.com/v1/'
