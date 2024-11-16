0dc76462a74e2521ac76f1e7380b38d6432c85e1
LenaWB-SOE <170402755+LenaWB-SOE@users.noreply.github.com>
2024-11-16 15:57:15 +0000

    edit to pir test

--- PIR_test.py
+++ PIR_test.py
@@ -1,8 +1,11 @@
 from gpiozero import MotionSensor
+import time
 
-pir = MotionSensor(4)
+pir = MotionSensor(4, None, None, 4)
 
 while True:
-	pir.wait_for_motion()
-	print("You moved")
-	pir.wait_for_no_motion()
+	# pir.wait_for_motion()
+	# print("You moved")
+	# pir.wait_for_no_motion()
+    print(pir.value)
+    time.sleep(0.01)
