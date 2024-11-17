01080c3f8f78893fc59c29ba1f6859d3e95bd2e3
LenaWB-SOE <170402755+LenaWB-SOE@users.noreply.github.com>
2024-11-17 11:05:45 +0000

    resistor instead of capacitor

--- LDR_test.py
+++ LDR_test.py
@@ -1,7 +1,23 @@
-from gpiozero import LightSensor, Buzzer
+# from gpiozero import LightSensor, Buzzer
+# import time
+
+# ldr = LightSensor(4)  # alter if using a different pin
+# while True:
+#     print(ldr.value)
+#     time.sleep(0.1)
+
+import RPi.GPIO as GPIO
 import time
 
-ldr = LightSensor(4)  # alter if using a different pin
-while True:
-    print(ldr.value)
-    time.sleep(0.1)
+LDR_PIN = 4  # Replace with your GPIO pin number
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LDR_PIN, GPIO.IN)
+
+try:
+    while True:
+        value = GPIO.input(LDR_PIN)
+        print(f"LDR Value: {value}")
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
