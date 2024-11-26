5e22775b11d261d449acdf5ce04ff20db2462578
LenaWB-SOE <170402755+LenaWB-SOE@users.noreply.github.com>
2024-11-26 16:22:58 +0000

    edit

--- LDR_test.py
+++ LDR_test.py
@@ -35,18 +35,18 @@ import time
 import board
 import busio
 from digitalio import DigitalInOut
-from adafruit_mcp3xxx.mcp3008 import MCP3008
+from adafruit_mcp3xxx.mcp3008 import MCP3008, P0
 from adafruit_mcp3xxx.analog_in import AnalogIn
-from gpiozero import MCP3008
+#from gpiozero import MCP3008
 
 # Set up SPI and MCP3008
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 cs = DigitalInOut(board.D5)  # Chip select pin (GPIO8 in Wiring)
-#mcp = MCP3008(spi, cs)
+mcp = MCP3008(spi, cs)
 
 # Connect LDR to channel 0 (CH0)
-#ldr = AnalogIn(mcp, MCP3008.P0)
-adc = MCP3008(channel=0, device=0)
+ldr = AnalogIn(mcp, P0)
+#adc = MCP3008(channel=0, device=0)
 
 # ldr = AnalogIn(mcp, 0)
 # value = ldr.read_adc(0)
@@ -56,11 +56,11 @@ print("Reading LDR values...")
 try:
     while True:
         # Read the raw value and voltage
-        print(str(adc.value*3.3))
-        # raw_value = ldr.value  # 0-65535
-        # voltage = ldr.voltage  # Voltage corresponding to the raw value
+        # print(str(adc.value*3.3))
+        raw_value = ldr.value  # 0-65535
+        voltage = ldr.voltage  # Voltage corresponding to the raw value
 
-        # print(f"Raw Value: {raw_value}, Voltage: {voltage:.2f} V")
+        print(f"Raw Value: {raw_value}, Voltage: {voltage:.2f} V")
         time.sleep(1)
 except KeyboardInterrupt:
     print("Exiting program.")
