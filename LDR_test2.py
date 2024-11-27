47b1ea51e7fa6fc00e873edd475b734c1dc50e44
LenaWB-SOE <170402755+LenaWB-SOE@users.noreply.github.com>
2024-11-27 08:47:05 +0000

    d22 to D24

--- LDR_test2.py
+++ LDR_test2.py
@@ -14,7 +14,8 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
 # create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D22)
+# cs = digitalio.DigitalInOut(board.D22)
+cs = digitalio.DigitalInOut(board.D24)
 
 # create the mcp object
 mcp = MCP.MCP3008(spi, cs)
