7ba2aad7813783ad73fbd594c72088daae621801
LenaWB-SOE <170402755+LenaWB-SOE@users.noreply.github.com>
2024-11-30 23:20:34 +0000

    edit

--- spotify_client.py
+++ spotify_client.py
@@ -74,8 +74,10 @@ class SpotifyClient:
         playlist_id = playlist_uri[17:]
         print(playlist_id)
         offset = 2
-        response = requests.get(f"{API_BASE_URL}playlists/{playlist_id}/tracks?offset={offset}&limit=1", headers=self.get_headers())
+        limit = 2
+        response = requests.get(f"{API_BASE_URL}playlists/{playlist_id}/tracks?offset={offset}&limit={limit}", headers=self.get_headers())
         #response = requests.get(f"{API_BASE_URL}/playlists/{playlist_id}/tracks", headers=self.get_headers())
+        print(response)
         if response.status_code == 204:
             print(response.json())
             #song = response.json()["items"][0]["track"]["uri"]
