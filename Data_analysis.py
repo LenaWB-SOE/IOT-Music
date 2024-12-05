9404d5de5f37995d20900e2a6d570171ea4b6265
LenaWB-SOE <170402755+LenaWB-SOE@users.noreply.github.com>
2024-12-05 15:26:24 +0000

    just importing pandas

--- Data_analysis.py
+++ Data_analysis.py
@@ -1,13 +1,13 @@
-# import pandas as pd
-
-# # Load data
-# data = pd.read_csv("ambient_data.csv")
-
-# # Display the first few rows
-# print(data.head())
-
-import numpy as np
 import pandas as pd
 
-print("Numpy version:", np.__version__)
-print("Pandas version:", pd.__version__)
+# Load data
+data = pd.read_csv("ambient_data.csv")
+
+# Display the first few rows
+print(data.head())
+
+# import numpy as np
+# import pandas as pd
+
+# print("Numpy version:", np.__version__)
+# print("Pandas version:", pd.__version__)
